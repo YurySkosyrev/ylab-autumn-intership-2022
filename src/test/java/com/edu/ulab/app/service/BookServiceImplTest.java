3426c5be240748fb8aa486ef5c0a0896f832d568
YurySkosyrev <skosyrev-87@mail.ru>
2022-10-03 09:59:16 +0300

    Homework 5

--- src/test/java/com/edu/ulab/app/service/BookServiceImplTest.java
+++ src/test/java/com/edu/ulab/app/service/BookServiceImplTest.java
@@ -3,8 +3,10 @@ package com.edu.ulab.app.service;
 import com.edu.ulab.app.dto.BookDto;
 import com.edu.ulab.app.entity.Book;
 import com.edu.ulab.app.entity.Person;
+import com.edu.ulab.app.exception.NotFoundException;
 import com.edu.ulab.app.mapper.BookMapper;
 import com.edu.ulab.app.repository.BookRepository;
+import com.edu.ulab.app.repository.UserRepository;
 import com.edu.ulab.app.service.impl.BookServiceImpl;
 import com.edu.ulab.app.service.impl.UserServiceImpl;
 import org.junit.jupiter.api.DisplayName;
@@ -16,8 +18,15 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.doNothing;
 
 /**
  * Тестирование функционала {@link com.edu.ulab.app.service.impl.BookServiceImpl}.
@@ -33,6 +42,9 @@ public class BookServiceImplTest {
     BookRepository bookRepository;
 
     @Mock
+    UserRepository userRepository;
+
+    @Mock
     BookMapper bookMapper;
 
     @Test
@@ -40,7 +52,7 @@ public class BookServiceImplTest {
     void saveBook_Test() {
         //given
         Person person  = new Person();
-        person.setId(1);
+        person.setId(1L);
 
         BookDto bookDto = new BookDto();
         bookDto.setUserId(1L);
@@ -62,7 +74,7 @@ public class BookServiceImplTest {
         book.setPerson(person);
 
         Book savedBook = new Book();
-        savedBook.setId(1);
+        savedBook.setId(1L);
         savedBook.setPageCount(1000);
         savedBook.setTitle("test title");
         savedBook.setAuthor("test author");
@@ -80,11 +92,225 @@ public class BookServiceImplTest {
         assertEquals(1L, bookDtoResult.getId());
     }
 
+    @Test
+    @DisplayName("Обновление книги. Должно пройти успешно.")
+    void updateBook_Test() {
 
-    // update
-    // get
-    // get all
-    // delete
+        //given
+        Person person  = new Person();
+        person.setId(1L);
+
+        BookDto bookUpdateDto = new BookDto();
+        bookUpdateDto.setUserId(1L);
+        bookUpdateDto.setAuthor("new author");
+        bookUpdateDto.setTitle("new title");
+        bookUpdateDto.setPageCount(1000);
+
+        Book updateBook = new Book();
+        updateBook.setId(1L);
+        updateBook.setPageCount(1000);
+        updateBook.setTitle("new title");
+        updateBook.setAuthor("new author");
+        updateBook.setPerson(person);
+
+        BookDto resultUpdateDto = new BookDto();
+        resultUpdateDto.setUserId(1L);
+        resultUpdateDto.setAuthor("new author");
+        resultUpdateDto.setTitle("new title");
+        resultUpdateDto.setPageCount(1000);
+
+        //when
+        when(bookMapper.bookDtoToBook(bookUpdateDto)).thenReturn(updateBook);
+        when(bookRepository.save(updateBook)).thenReturn(updateBook);
+        when(bookMapper.bookToBookDto(updateBook)).thenReturn(resultUpdateDto);
+
+        //then
+        BookDto result = bookService.updateBook(bookUpdateDto);
+        assertEquals("new author", result.getAuthor());
+        assertEquals("new title", result.getTitle());
+    }
+
+    @Test
+    @DisplayName("Получение книги. Должно пройти успешно.")
+    void getBook_Test() {
+        //given
+        Person person  = new Person();
+        person.setId(1L);
+
+
+        BookDto result = new BookDto();
+        result.setId(1L);
+        result.setUserId(1L);
+        result.setAuthor("test author");
+        result.setTitle("test title");
+        result.setPageCount(1000);
+
+        Book savedBook = new Book();
+        savedBook.setId(1L);
+        savedBook.setPageCount(1000);
+        savedBook.setTitle("test title");
+        savedBook.setAuthor("test author");
+        savedBook.setPerson(person);
+
+        Long bookId = 1L;
+
+        //when
+        when(bookRepository.findById(bookId)).thenReturn(Optional.of(savedBook));
+        when(bookMapper.bookToBookDto(savedBook)).thenReturn(result);
+
+        //then
+        BookDto getBookDto = bookService.getBookById(bookId);
+        assertEquals(1L, getBookDto.getId());
+        assertEquals(1L, getBookDto.getUserId());
+        assertEquals(1000, getBookDto.getPageCount());
+        assertEquals("test title", getBookDto.getTitle());
+        assertEquals("test author", getBookDto.getAuthor());
+    }
+
+    @Test
+    @DisplayName("Получение всех книг по id юзера. Должно пройти успешно.")
+    void getAllBook_Test() {
+
+        //given
+        Person person  = new Person();
+        person.setId(1L);
+
+        BookDto result1 = new BookDto();
+        result1.setId(1L);
+        result1.setUserId(1L);
+        result1.setAuthor("author1");
+        result1.setTitle("title1");
+        result1.setPageCount(1000);
+
+        Book savedBook1 = new Book();
+        savedBook1.setId(1L);
+        savedBook1.setPageCount(1000);
+        savedBook1.setTitle("title1");
+        savedBook1.setAuthor("author1");
+        savedBook1.setPerson(person);
+
+        BookDto result2 = new BookDto();
+        result2.setId(2L);
+        result2.setUserId(1L);
+        result2.setAuthor("author2");
+        result2.setTitle("title2");
+        result2.setPageCount(1000);
+
+        Book savedBook2 = new Book();
+        savedBook2.setId(2L);
+        savedBook2.setPageCount(1000);
+        savedBook2.setTitle("title2");
+        savedBook2.setAuthor("author2");
+        savedBook2.setPerson(person);
+
+
+        List<Book> personBooks = new ArrayList<>();
+        personBooks.add(savedBook1);
+        personBooks.add(savedBook2);
+
+
+        //when
+        when(bookMapper.bookToBookDto(savedBook1)).thenReturn(result1);
+        when(bookMapper.bookToBookDto(savedBook2)).thenReturn(result2);
+        when(bookRepository.findAllByPersonId(person.getId())).thenReturn(personBooks);
+
+
+        //then
+        List<Book> books = bookService.getBooksByPersonId(person.getId());
+        assertEquals(books.size(), 2);
+        assertEquals("title1", books.get(0).getTitle());
+        assertEquals("title2", books.get(1).getTitle());
+
+    }
+
+    @Test
+    @DisplayName("Удаление книги. Должно пройти успешно.")
+    void deleteBook_Test() {
+
+        //given
+
+        Long id = 1L;
+
+        //when
+        doNothing().when(bookRepository).deleteById(id);
+
+        //then
+        bookService.deleteBookById(id);
+    }
+
 
     // * failed
+    @Test
+    @DisplayName("Ошибка при выдачи книги. Должно пройти успешно.")
+    void failedGetBook_Test() {
+
+        //given
+
+        Person person  = new Person();
+        person.setId(1L);
+
+
+        BookDto result = new BookDto();
+        result.setId(1L);
+        result.setUserId(1L);
+        result.setAuthor("test author");
+        result.setTitle("test title");
+        result.setPageCount(1000);
+
+        Book savedBook = new Book();
+        savedBook.setId(1L);
+        savedBook.setPageCount(1000);
+        savedBook.setTitle("test title");
+        savedBook.setAuthor("test author");
+        savedBook.setPerson(person);
+
+        Long bookId = 1L;
+
+        //when
+        when(bookRepository.findById(bookId)).thenThrow(new NotFoundException("Book with id: 1 not found"));
+        when(bookMapper.bookToBookDto(savedBook)).thenReturn(result);
+
+        //then
+        assertThatThrownBy(() -> bookService.getBookById(bookId))
+                .isInstanceOf(NotFoundException.class)
+                .hasMessage("Book with id: 1 not found");
+    }
+
+    @Test
+    @DisplayName("Ошибка при выдачи книги. Должно пройти успешно.")
+    void failedGetBook1_Test() {
+        //given
+
+        Person person  = new Person();
+        person.setId(1L);
+
+
+        BookDto result = new BookDto();
+        result.setId(1L);
+        result.setUserId(1L);
+        result.setAuthor("test author");
+        result.setTitle("test title");
+        result.setPageCount(1000);
+
+        Book savedBook = new Book();
+        savedBook.setId(1L);
+        savedBook.setPageCount(1000);
+        savedBook.setTitle("test title");
+        savedBook.setAuthor("test author");
+        savedBook.setPerson(person);
+
+        Long bookId = 1L;
+
+
+        //when
+
+        when(bookRepository.findById(bookId)).thenReturn(Optional.empty());
+        when(bookMapper.bookToBookDto(savedBook)).thenReturn(result);
+
+
+        //then
+        assertThatThrownBy(() -> bookService.getBookById(bookId))
+                .isInstanceOf(NotFoundException.class)
+                .hasMessage("Book with id 1 is not found");
+    }
 }
--- src/test/java/com/edu/ulab/app/service/UserServiceImplTest.java
+++ src/test/java/com/edu/ulab/app/service/UserServiceImplTest.java
@@ -4,6 +4,7 @@ import com.edu.ulab.app.dto.BookDto;
 import com.edu.ulab.app.dto.UserDto;
 import com.edu.ulab.app.entity.Book;
 import com.edu.ulab.app.entity.Person;
+import com.edu.ulab.app.exception.NotFoundException;
 import com.edu.ulab.app.mapper.UserMapper;
 import com.edu.ulab.app.repository.UserRepository;
 import com.edu.ulab.app.service.impl.UserServiceImpl;
@@ -16,7 +17,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
 /**
@@ -51,7 +56,7 @@ public class UserServiceImplTest {
         person.setTitle("test title");
 
         Person savedPerson  = new Person();
-        savedPerson.setId(1);
+        savedPerson.setId(1L);
         savedPerson.setFullName("test name");
         savedPerson.setAge(11);
         savedPerson.setTitle("test title");
@@ -76,10 +81,120 @@ public class UserServiceImplTest {
         assertEquals(1L, userDtoResult.getId());
     }
 
-    // update
-    // get
-    // get all
-    // delete
+    @Test
+    @DisplayName("Обновление пользователя. Должно пройти успешно.")
+    void updatePerson_Test() {
+
+        //given
+        UserDto userUpdateDto = new UserDto();
+        userUpdateDto.setId(1L);
+        userUpdateDto.setAge(11);
+        userUpdateDto.setFullName("new name");
+        userUpdateDto.setTitle("test title");
+
+        Person updatePerson  = new Person();
+        updatePerson.setId(1L);
+        updatePerson.setFullName("new name");
+        updatePerson.setAge(11);
+        updatePerson.setTitle("test title");
+
+        UserDto updateResult = new UserDto();
+        updateResult.setId(1L);
+        updateResult.setAge(11);
+        updateResult.setFullName("new name");
+        updateResult.setTitle("test title");
+
+        //when
+        when(userMapper.userDtoToPerson(userUpdateDto)).thenReturn(updatePerson);
+        when(userRepository.findById(updatePerson.getId())).thenReturn(Optional.of(updatePerson));
+        when(userRepository.save(updatePerson)).thenReturn(updatePerson);
+        when(userMapper.personToUserDto(updatePerson)).thenReturn(updateResult);
+
+        //then
+        UserDto result = userService.updateUser(userUpdateDto);
+        assertEquals("new name", result.getFullName());
+    }
+
+    @Test
+    @DisplayName("Получение пользователя. Должно пройти успешно.")
+    void getPerson_Test() {
+        //given
+        Long personId = 1L;
+
+        Person savedPerson  = new Person();
+        savedPerson.setId(1L);
+        savedPerson.setFullName("old name");
+        savedPerson.setAge(11);
+        savedPerson.setTitle("test title");
+
+        UserDto result = new UserDto();
+        result.setId(1L);
+        result.setAge(11);
+        result.setFullName("old name");
+        result.setTitle("test title");
+
+
+        //when
+        when(userRepository.findById(personId)).thenReturn(Optional.of(savedPerson));
+        when(userMapper.personToUserDto(savedPerson)).thenReturn(result);
+
+        //then
+        UserDto getUserDto = userService.getUserById(savedPerson.getId());
+
+        assertEquals(1L, getUserDto.getId());
+        assertEquals(11L, getUserDto.getAge());
+        assertEquals("old name", getUserDto.getFullName());
+        assertEquals("test title", getUserDto.getTitle());
+    }
+
+
+    @Test
+    @DisplayName("Удаление пользователя. Должно пройти успешно.")
+    void deletePerson_Test() {
+        //given
+        Long personId = 1L;
+
+
+        //when
+        doNothing().when(userRepository).deleteById(personId);
+
+
+        //then
+        userService.deleteUserById(personId);
+    }
+
+    // * failed
+    @Test
+    @DisplayName("Ошибка при получении пользователя. Должно пройти успешно.")
+    void failedGetPerson_Test() {
+        //given
+        Long userId = 1L;
+
+        Person savedPerson  = new Person();
+        savedPerson.setId(1L);
+        savedPerson.setFullName("test name");
+        savedPerson.setAge(11);
+        savedPerson.setTitle("test title");
+
+        UserDto result = new UserDto();
+        result.setId(1L);
+        result.setAge(11);
+        result.setFullName("test name");
+        result.setTitle("test title");
+
+
+        //when
+        when(userRepository.findById(userId)).thenReturn(Optional.empty());
+        when(userMapper.personToUserDto(savedPerson)).thenReturn(result);
+
+
+        //then
+
+        assertThatThrownBy(() -> userService.getUserById(userId))
+                .isInstanceOf(NotFoundException.class)
+                .hasMessage("User with id 1 is not found");
+    }
+
 
     // * failed
     //         doThrow(dataInvalidException).when(testRepository)
