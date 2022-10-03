ef474aae5ebcd0e5bf0fd7da7d9c70edc987c67f
YurySkosyrev <skosyrev-87@mail.ru>
2022-10-03 10:18:11 +0300

    Homework 5

--- src/test/java/com/edu/ulab/app/repository/BookRepositoryTest.java
+++ src/test/java/com/edu/ulab/app/repository/BookRepositoryTest.java
@@ -12,6 +12,9 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.jdbc.Sql;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static com.vladmihalcea.sql.SQLStatementCountValidator.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -30,7 +33,8 @@ public class BookRepositoryTest {
         SQLStatementCountValidator.reset();
     }
 
-    @DisplayName("Сохранить книгу и автора. Число select должно равняться 1")
+
+    @DisplayName("Сохранить книгу и автора. Число select должно равняться 2")
     @Test
     @Rollback
     @Sql({"classpath:sql/1_clear_schema.sql",
@@ -65,10 +69,112 @@ public class BookRepositoryTest {
         assertDeleteCount(0);
     }
 
-    // update
-    // get
-    // get all
-    // delete
+
+    @DisplayName("Обновить книгу. Число select должно равняться 1")
+    @Test
+    @Rollback
+    @Sql({"classpath:sql/1_clear_schema.sql",
+            "classpath:sql/2_insert_person_data.sql",
+            "classpath:sql/3_insert_book_data.sql"
+    })
+    void updateBook_thenAssertDmlCount() {
+
+        //Given
+        Book book = new Book();
+        book.setId(2002L);
+        book.setAuthor("Test Author");
+        book.setTitle("test");
+        book.setPageCount(1000);
+
+        //When
+        Book result = bookRepository.save(book);
+
+        //Then
+        assertThat(result.getPageCount()).isEqualTo(1000);
+        assertThat(result.getTitle()).isEqualTo("test");
+        assertSelectCount(1);
+        assertInsertCount(0);
+        assertUpdateCount(0);
+        assertDeleteCount(0);
+    }
+
+
+    @DisplayName("Получить книгу по id. Число select должно равняться 1")
+    @Test
+    @Rollback
+    @Sql({"classpath:sql/1_clear_schema.sql",
+            "classpath:sql/2_insert_person_data.sql",
+            "classpath:sql/3_insert_book_data.sql"
+    })
+    void getBook_thenAssertDmlCount() {
+
+        //Given
+        Long BOOK_ID = 2002L;
+
+        //When
+        Book result = bookRepository.findById(BOOK_ID).orElse(new Book());
+
+        //Then
+        assertThat(result.getAuthor()).isEqualTo("author");
+        assertThat(result.getPageCount()).isEqualTo(5500);
+        assertSelectCount(1);
+        assertInsertCount(0);
+        assertUpdateCount(0);
+        assertDeleteCount(0);
+    }
+
+
+    @DisplayName("Получить все книги по id автора. Число select должно равняться 1")
+    @Test
+    @Rollback
+    @Sql({"classpath:sql/1_clear_schema.sql",
+            "classpath:sql/2_insert_person_data.sql",
+            "classpath:sql/3_insert_book_data.sql"
+    })
+    void getAllBookByPersonId_thenAssertDmlCount() {
+
+        //Given
+        Long PERSON_ID = 1001L;
+
+        //When
+        List<Book> books = bookRepository.findAllByPersonId(PERSON_ID);
+
+        //Then
+        assertThat(books.size()).isEqualTo(2);
+        assertThat(books.get(0).getPageCount()).isEqualTo(5500);
+        assertThat(books.get(1).getPageCount()).isEqualTo(6655);
+        assertSelectCount(1);
+        assertInsertCount(0);
+        assertUpdateCount(0);
+        assertDeleteCount(0);
+    }
+
+
+    @DisplayName("Удалить книгу. Число select должно равняться 1")
+    @Test
+    @Rollback
+    @Sql({"classpath:sql/1_clear_schema.sql",
+            "classpath:sql/2_insert_person_data.sql",
+            "classpath:sql/3_insert_book_data.sql"
+    })
+    void deleteBook_thenAssertDmlCount() {
+
+        //Given
+        Long BOOK_ID = 2002L;
+
+        //When
+        bookRepository.deleteById(BOOK_ID);
+        Book result = bookRepository.findById(BOOK_ID).orElse(new Book());
+
+        //Then
+        assertThat(result.getId()).isEqualTo(null);
+        assertThat(result.getAuthor()).isEqualTo(null);
+        assertSelectCount(1);
+        assertInsertCount(0);
+        assertUpdateCount(0);
+        assertDeleteCount(0);
+    }
+
 
     // * failed
 
